107c29c248518e64e02d2882c1ec899c27aaebfb
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2023-12-28 17:18:22 +0200

    created Interface for JsonPlaceholderClient

---
+++ ItemShop/Clients/IJsonPlaceholderClient.cs
@@ -0,0 +1,11 @@
+﻿using ItemShop.Models.DTOs.UserDtos;
+
+namespace ItemShop.Clients
+{
+    public interface IJsonPlaceholderClient
+    {
+        Task<JsonPlaceholderResult<UserDto>> CreateUserAsync(CreateUserDto user);
+        Task<JsonPlaceholderResult<UserDto>> GetUserAsync(int userId);
+        Task<JsonPlaceholderResult<List<UserDto>>> GetUsersAsync();
+    }
+}
--- ItemShop/Program.cs
+++ ItemShop/Program.cs
@@ -24,21 +24,12 @@ builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql(dbConnectionString))
 
 builder.Services.AddTransient<ItemService>();
 builder.Services.AddTransient<IEFItemRepository, EFItemRepository>();
-builder.Services.AddTransient<JsonPlaceholderClient>();
+builder.Services.AddTransient<IJsonPlaceholderClient, JsonPlaceholderClient>();
 builder.Services.AddTransient<UserService>();
 
 builder.Services.AddHttpClient();
 
 builder.Services.AddAutoMapper(typeof(Program));
-
-//builder.Services.AddTransient<IDbConnection>(sp => new NpgsqlConnection(dbConnectionString));
-
-//EnsureDatabase.For.PostgresqlDatabase(dbConnectionString);
-//var upgrader =
-//    DeployChanges.To.PostgresqlDatabase(dbConnectionString)
-//    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-//    .LogToNowhere().Build();
-//var result = upgrader.PerformUpgrade();
 
 var app = builder.Build();
 
