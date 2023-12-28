701fbc1b54913aee898f99fb8e3272e1050e268f
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2023-12-28 19:52:23 +0200

    created shop repository

---
+++ ItemShop/Repositories/ShopRepository.cs
@@ -0,0 +1,38 @@
+﻿using ItemShop.Contexts;
+using ItemShop.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemShop.Repositories
+{
+    public class ShopRepository
+    {
+        private readonly DataContext _dataContext;
+        public ShopRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<List<Shop>> Get()
+        {
+            return await _dataContext.Shops.ToListAsync();
+        }
+        public async Task Create(Shop shop)
+        {
+            _dataContext.Shops.Add(shop);
+            await _dataContext.SaveChangesAsync();
+        }
+        public async Task<Shop> Get(int Id)
+        {
+            return await _dataContext.Shops.AsNoTracking().FirstOrDefaultAsync(x => x.Id == Id);
+        }
+        public async Task Delete(Shop shop)
+        {
+            _dataContext.Remove(shop);
+            await _dataContext.SaveChangesAsync();
+        }
+        public async Task Update(Shop shop)
+        {
+            _dataContext.Update(shop);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
